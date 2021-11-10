713635ff355e7c6cee9942c2f4e12b1d2408b313
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2021-11-10 16:13:54 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -34,9 +34,9 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
-        print("🔰ՏTᗩᖇT ᖇᗴᗷᗴᒪᗷOT🔰")
+        print("🔰𝐒𝐭𝐚𝐫𝐭 𝐋𝐲𝐧𝐱𝐁𝐨𝐭🔰")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("⚡ᖇᗴᗷᗴᒪᗷOT ՏTᗩᖇTᑌᑭ ᑕOᗰᑭᒪᗴTᗴᗪ⚡")
+        print("⚡𝐋𝐲𝐧𝐱𝐁𝐨𝐭 𝐬𝐭𝐚𝐫𝐭𝐮𝐩 𝐜𝐨𝐦𝐩𝐥𝐞𝐭𝐞⚡")
     else:
         bot.start()
 
@@ -52,7 +52,7 @@ for name in files:
 
 import userbot._core
 
-print(f"""Hello sir i am REBELBOT!! REBELBOT VERSION :- {REBELversion} YOUR REBELBOT IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @REBELBOT_SUPPORT .""")
+print(f"""𝐇𝐞𝐥𝐥𝐨 𝐬𝐢𝐫 𝐢 𝐚𝐦 𝐋𝐘𝐍𝐗𝐁𝐎𝐓!! 𝐋𝐘𝐍𝐗𝐁𝐎𝐓𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :- {REBELversion} 𝐘𝐎𝐔𝐑 𝐋𝐘𝐍𝐗𝐁𝐎𝐓 𝐈𝐒 𝐑𝐄𝐀𝐃𝐘! 𝐅𝐎𝐑 𝐂𝐇𝐄𝐂𝐊 𝐘𝐎𝐔𝐑 𝐁𝐎𝐓 𝐖𝐎𝐑𝐊𝐈𝐍𝐆 𝐎𝐑 𝐍𝐎𝐓 𝐏𝐋𝐄𝐀𝐒𝐄 𝐓𝐘𝐏𝐄 (.𝐚𝐥𝐢𝐯𝐞/.𝐩𝐢𝐧𝐠) 𝐄𝐍𝐉𝐎𝐘 𝐘𝐎𝐔𝐑 𝐁𝐎𝐓! 𝐉𝐎𝐈𝐍 𝐅𝐎𝐑 𝐌𝐎𝐑𝐄 𝐅𝐔𝐓𝐔𝐑𝐄 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 @𝐋𝐘𝐍𝐗𝐁𝐎𝐓_𝐂𝐇𝐀𝐓 .""")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
